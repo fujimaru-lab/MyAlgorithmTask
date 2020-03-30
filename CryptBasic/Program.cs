15aca004fd4ef68133a534da5071354fdad51390
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-03-30 11:56:09 +0900

    暗号化基礎、順位付け

---
+++ CryptBasic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CryptBasic
+{
+    class Program
+    {
+        private static string[] table = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O",
+                                         "P", "A", "S", "D", "F", "G", "H", "J", "K",
+                                         "L", "Z", "X", "C", "V", "B", "N", "M", "?"};
+        private static char[] baseTable = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                                             'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                                             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        static void Main(string[] args)
+        {
+            string data = "KSOIDHEPZ";
+            foreach( var chr in data)
+            {
+                bool isExist = false;
+                int index = table.Length - 1;
+                for(var i = 0; i < baseTable.Length; i++)
+                {
+                    isExist = chr == baseTable[i];
+                    if (isExist)
+                    {
+                        index = i;
+                        i = baseTable.Length;
+                    }
+                }
+                Console.Write(table[index]);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Rank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rank
+{
+    class Program
+    {
+        private static int[] points = { 56, 25, 67, 88, 100, 61, 55, 67, 76, 56 };
+
+        static void Main(string[] args)
+        {
+            foreach(var point in points)
+            {
+                Console.WriteLine("Point:{0, 3:D} - Rank:{1, 2:D}", point, CalcRank(points, point));
+            }
+            Console.ReadKey();
+        }
+
+        private static int CalcRank(int[] points, int stdPoint)
+        {
+            int rank = 1;
+            foreach(var point in points)
+            {
+                if(stdPoint < point)
+                {
+                    rank++;
+                }
+            }
+            return rank;
+        }
+    }
+}
