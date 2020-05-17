00fc5a47f1c8a82df8958067e0ad74c90ed3944e
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2020-05-17 20:50:43 +0900

    [new]ランダムな数列の生成処理(疑似乱数を使用)

---
+++ lang_golang/random_permutation/cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// MAX 作成する数列の最大値
+const MAX int = 32
+
+func main() {
+	// ランダム数列
+	rndPerm := make([]int, MAX)
+
+	// 数列の初期化
+	for i := 0; i < MAX; i++ {
+		rndPerm[i] = i + 1
+	}
+
+	fmt.Println("処理前:", rndPerm)
+
+	// ランダムな順列の生成処理
+	for n := MAX; n >= 1; n-- {
+		// 処理対象の配列の位置を疑似乱数で指定
+		index := rand.Int31n(int32(n))
+
+		// 配列の中身を相互に置換
+		tmp := rndPerm[index]
+		rndPerm[index] = rndPerm[n-1]
+		rndPerm[n-1] = tmp
+	}
+
+	fmt.Println("処理後:", rndPerm)
+}
